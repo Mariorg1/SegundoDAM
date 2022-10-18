f198f0ac302e9c4fd0591850fd4effb3d05bcbc4
juanfran <juanfranciscogutierrez93@gmail.com>
2022-10-18 12:23:14 +0200

    Hilo con pausa (.sleep)

---
+++ src/hilos_clase/SleepMessages.java
@@ -0,0 +1,14 @@
+package hilos_clase;
+
+public class SleepMessages {
+	public static void main(String args[]) throws InterruptedException {
+		String importantInfo[] = { "Mario le falla el tobillo", "Javi pillo portatil", "Rafa tiene teclado numerico",
+				"Raul tiene una moto doblada" };
+		for (int i = 0; i < importantInfo.length; i++) {
+			// Pausa de 4 segundos
+			Thread.sleep(4000);
+			// Mostramos por pantalla
+			System.out.println(importantInfo[i]);
+		}
+	}
+}
