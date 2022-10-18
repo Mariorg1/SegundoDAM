3fbf88ecb956006fc9df44c17caa87fad9d1a746
Mario <mariorrgg1@gmail.com>
2022-10-18 12:31:05 +0200

    tobillo derecho

--- src/hilos_clase/SleepMessages.java
+++ src/hilos_clase/SleepMessages.java
@@ -2,7 +2,7 @@ package hilos_clase;
 
 public class SleepMessages {
 	public static void main(String args[]) throws InterruptedException {
-		String importantInfo[] = { "Mario le falla el tobillo", "Javi pillo portatil", "Rafa tiene teclado numerico",
+		String importantInfo[] = { "Mario le falla el tobillo derecho", "Javi pillo portatil", "Rafa tiene teclado numerico",
 				"Raul tiene una moto doblada" };
 		for (int i = 0; i < importantInfo.length; i++) {
 			// Pausa de 4 segundos
