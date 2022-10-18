ce80718ff99cc8a2b241822221a741d188df30df
juanfran <juanfranciscogutierrez93@gmail.com>
2022-10-18 12:46:08 +0200

    voy

--- src/hilos_clase/SleepMessages.java
+++ src/hilos_clase/SleepMessages.java
@@ -7,7 +7,7 @@ public class SleepMessages {
 		for (int i = 0; i < importantInfo.length; i++) {
 			// Pausa de 4 segundos
 			Thread.sleep(4000);
-			// Mostramos por pantalla
+			// Mostramos por pantallas
 			System.out.println(importantInfo[i]);
 		}
 	}
